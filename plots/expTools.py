f0319247764762b0120ae1dc434e7160982de694
Raymond Namyst <gforgeron@gmail.com>
2020-02-27 13:55:02 +0100

    fixed version mismatch

--- plots/expTools.py
+++ plots/expTools.py
@@ -2,25 +2,27 @@ import os
 from itertools import *
 import subprocess
 
+
 def iterateur_option(dicopt):
     options = []
-    for opt,listval in dicopt.items():
+    for opt, listval in dicopt.items():
         optlist = []
         for val in listval:
             optlist += [opt + str(val)]
         options += [optlist]
-        #print(options)
+        # print(options)
     for value in product(*options):
         #print("value :", value)
         yield ' '.join(value)
 
-def execute(commande, ompenv, option, nb = 1, verbose = True, easyPath='.') :
+
+def execute(commande, ompenv, option, nbrun=1, verbose=True, easyPath='.'):
     path = os.getcwd()
-    os.chdir(easyPath);
-    for omp in iterateur_option(ompenv) :
+    os.chdir(easyPath)
+    for omp in iterateur_option(ompenv):
         for opt in iterateur_option(option):
-            for i in range(nb) :
-                if (verbose) :
+            for i in range(nbrun):
+                if (verbose):
                     print(omp + " " + commande + " -n " + opt)
                 if(subprocess.call([omp + " " + commande + " -n " + opt], shell=True) == 1):
                     os.chdir(path)
