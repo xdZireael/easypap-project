d9057c6a66408336606b49a4d72347ca90bd14b4
Raymond Namyst <gforgeron@gmail.com>
2023-02-24 09:47:47 +0100

    output file explicitely specified in run-xp-scrollup launch script

--- plots/run-xp-scrollup.py
+++ plots/run-xp-scrollup.py
@@ -3,15 +3,16 @@ from graphTools import *
 from expTools import *
 import os
 
-easyspap_options = {}
-easyspap_options["--kernel "] = ["scrollup"]
-easyspap_options["--variant "] = ["seq", "ji"]
+easypap_options = {}
+easypap_options["--kernel "] = ["scrollup"]
+easypap_options["--variant "] = ["seq", "ji"]
+easypap_options["--output-file "] = ["scrollup.csv"]
 
 omp_icv = {}  # OpenMP Internal Control Variables
 omp_icv["OMP_NUM_THREADS="] = [1]
 omp_icv["OMP_SCHEDULE="] = ["static"]
 
 for size in [2**i for i in range(5, 13)]:
-    easyspap_options["--iterations "] = [4096*4096//(size*size)]
-    easyspap_options["--size "] = [size]
-    execute('./run', omp_icv, easyspap_options, nbrun=5)
+    easypap_options["--iterations "] = [4096*4096//(size*size)]
+    easypap_options["--size "] = [size]
+    execute('./run', omp_icv, easypap_options, nbrun=5)
