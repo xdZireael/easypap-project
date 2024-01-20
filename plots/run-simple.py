1eb8cd8469c6a664bcf4c8f8519be54635be1a78
Pierre-André Wacrenier <wacrenier@labri.fr>
2024-01-20 22:47:32 +0100

    add recommended easyplot's parameter & use os.cpu_count() rather than  constant number

--- plots/run-simple.py
+++ plots/run-simple.py
@@ -4,24 +4,24 @@ from graphTools import *
 from expTools import *
 import os
 
+
 # options communes
 options = {}
 options["--label"] = ["final"]
 options["-k"] = ["mandel"]
-options["-i"] = [10]
-options["-of"] = ["./data/perf/data.csv"]
+options["-i"] = [5]
+options["-of"] = ["mandel-simple.csv"]
 options["-s"] = [512]
 
 ompenv = {}
-ompenv["OMP_NUM_THREADS"] = [1] + list(range(2, 13, 2))
-
-nbruns = 15
+ompenv["OMP_NUM_THREADS"] = [1] + list(range(4, os.cpu_count() + 1, 4))
+nbruns = 5
 
 options["-v"] = ["omp_tiled"]
 options["-ts"] = [8, 16, 32]
 ompenv["OMP_SCHEDULE"] = ["dynamic"]
 
-#execute('./run ', ompenv, options, nbruns, verbose=False, easyPath=".")
+# execute('./run ', ompenv, options, nbruns, verbose=False, easyPath=".")
 
 # OMP_LINE static
 del options["-ts"]
@@ -34,3 +34,6 @@ execute("./run ", ompenv, options, nbruns, verbose=False, easyPath=".")
 # OMP_LINE dynamic
 ompenv["OMP_SCHEDULE"] = ["dynamic"]
 execute("./run ", ompenv, options, nbruns, verbose=False, easyPath=".")
+
+print("Recommended plot:")
+print("plots/easyplot.py -if mandel-simple.csv -v omp_tiled")
--- plots/run-xp-scrollup.py
+++ plots/run-xp-scrollup.py
@@ -6,12 +6,17 @@ import os
 easyspap_options = {}
 easyspap_options["--kernel"] = ["scrollup"]
 easyspap_options["--variant"] = ["seq", "ji"]
+easyspap_options["-of"] = ["scrollup.csv"]
+
 
 omp_icv = {}  # OpenMP Internal Control Variables
 omp_icv["OMP_NUM_THREADS"] = [1]
 omp_icv["OMP_SCHEDULE"] = ["static"]
 
 for size in [2**i for i in range(5, 13)]:
-    easyspap_options["--iterations"] = [4096*4096//(size*size)]
+    easyspap_options["--iterations"] = [4096 * 4096 // (size * size)]
     easyspap_options["--size"] = [size]
-    execute('./run', omp_icv, easyspap_options, nbruns=5)
+    execute("./run", omp_icv, easyspap_options, nbruns=5)
+
+print("Recommended plot:")
+print("plots/easyplot.py -if scrollup.csv -x size -y time --xscale log2 --delete iterations tileh tilew")
