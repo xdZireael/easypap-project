f48826a14350bc2fb6f31e87855aae4563a75c83
Raymond Namyst <gforgeron@gmail.com>
2024-09-10 18:31:41 +0200

    minor

--- kernel/c/sample.c
+++ kernel/c/sample.c
@@ -1,6 +1,23 @@
 
 #include "easypap.h"
 
+// If defined, the initialization hook function is called quite early in the
+// initialization process, after the size (DIM variable) of images is known.
+// This function can typically spawn a team of threads, or allocated additionnal
+// OpenCL buffers.
+// A function named <kernel>_init_<variant> is searched first. If not found, a
+// function <kernel>_init is searched in turn.
+void sample_init (void)
+{
+  PRINT_DEBUG ('u', "Image size is %dx%d\n", DIM, DIM);
+  PRINT_DEBUG ('u', "Tile size is %dx%d\n", TILE_W, TILE_H);
+  PRINT_DEBUG ('u', "Press <SPACE> to pause/unpause, <ESC> to quit.\n");
+}
+
+// The image is a two-dimension array of size of DIM x DIM. Each pixel is of
+// type 'unsigned' and store the color information following a RGBA layout (4
+// bytes). Pixel at line 'l' and column 'c' in the current image can be accessed
+// using cur_img (l, c).
 
 int sample_do_tile_default (int x, int y, int width, int height)
 {
@@ -10,6 +27,9 @@ int sample_do_tile_default (int x, int y, int width, int height)
 
   return 0;
 }
+
+// The kernel returns 0, or the iteration step at which computation has
+// completed (e.g. stabilized).
 
 ///////////////////////////// Simple sequential version (seq)
 // Suggested cmdline(s):
