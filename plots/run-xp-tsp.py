8decc5cd08b56ffa864ba4fe32cb22014672172d
Pierre-André Wacrenier <wacrenier@labri.fr>
2025-01-28 14:07:27 +0100

    eval ompfor in run-xp

--- plots/run-xp-tsp.py
+++ plots/run-xp-tsp.py
@@ -8,7 +8,26 @@ easypapOptions = {
     "-of": ["tsp.csv"],
     "-k": ["tsp"],
     "-i": [5],
-    "-v": [f"ompcol2 -a {nb_villes}-2", f"ompcol3 -a {nb_villes}-3", f"ompcol4 -a {nb_villes}-4"],
+    "-v": [f"ompfor -a {nb_villes}-1", f"ompfor -a {nb_villes}-2"],
+    "--label": ["gnu"],
+}
+
+# OMP Internal Control Variable
+ompICV = {
+    "OMP_SCHEDULE": ["dynamic"],
+    "OMP_NUM_THREADS": [os.cpu_count()-2],
+}
+
+nbruns = 1
+# Lancement des experiences
+execute("./run ", ompICV, easypapOptions, nbruns, verbose=False, easyPath=".")
+
+
+easypapOptions = {
+    "-of": ["tsp.csv"],
+    "-k": ["tsp"],
+    "-i": [5],
+    "-v": [f"ompcol2 -a {nb_villes}-2" f"ompcol2 -a {nb_villes}-2", f"ompcol3 -a {nb_villes}-3", f"ompcol4 -a {nb_villes}-4"],
     "--label": ["gnu"],
 }
 
